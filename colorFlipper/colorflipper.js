daea303a58a157ed962e293864d4171c4e01a1fe
worldWeb <esbaev.nurdos@bk.ru>
2024-03-29 12:59:37 +0600

    arkin

---
+++ colorFlipper/colorflipper.js
@@ -0,0 +1,24 @@
+// const btn = document.querySelector(".btn");
+// const hexCode = document.querySelector(".hexCode");
+
+// const randomColorCode = ["#BBA6DD", "#FFB88A", "#FBC2C2", "#B4CFA4", "#A0C5E3"];
+
+// btn.addEventListener("click", () => {
+//   const random =
+//     randomColorCode[Math.floor(Math.random() * randomColorCode.length)];
+//   document.body.style.background = random;
+
+//   hexCode.textContent = random;
+// });
+
+const btn = document.querySelector(".btn");
+const hexCode = document.querySelector(".hexCode");
+
+const list = ["#BBA6DD", "#FFB88A", "#FBC2C2", "#B4CFA4", "#A0C5E3"];
+
+btn.addEventListener("click", () => {
+  const random = list[Math.floor(Math.random() * list.length)];
+  document.body.style.background = random;
+
+  hexCode.textContent = random;
+});
---
+++ darkMode/darkmode.js
@@ -0,0 +1,26 @@
+// function darkMode() {
+//   let element = document.body;
+//   let content = document.getElementById("DarkMode");
+//   element.className = "dark-mod";
+//   content.innerText = "Dark Mode is ON";
+// }
+// function lightMode() {
+//   let element = document.body;
+//   let content = document.getElementById("DarkMode");
+//   element.className = "light-mod";
+//   content.innerText = "Dark Mode is OFF";
+// }
+
+function darkMode() {
+  let back = document.body;
+  let button = document.getElementById("DarkMode");
+  back.className = "dark-mod";
+  button.innerText = "Dark Mode is ON";
+}
+
+function lightMode() {
+  let back = document.body;
+  let button = document.getElementById("DarkMode");
+  back.className = "light-mod";
+  button.innerText = "Dark Mode is OFF";
+}
